70fb4fa7795875605991af1a6d7c5cd0a2c1b4f1
Rowdy <mroddy5280@gmail.com>
2022-10-01 03:17:36 -0600

    Revert "Merge branch 'main' of personal.github.com:semaphore-protocol/taz-apps into feat/booth-display-page"
    This reverts commit c557bd3be7c010aed1c3248cb001349be9d2c093, reversing
    changes made to 43fb8d0cae07cd0f42bc8bac5818cb0ea12c6185.

--- apps/web-app/components/FAQ/Accordion.js
+++ apps/web-app/components/FAQ/Accordion.js
@@ -8,89 +8,22 @@ const Accordion = () => {
         {
             id: 1,
             question: "What is a Semaphore ID?",
-            answer: (
-                <p>
-                    Semaphore IDs allow users to generate zero-knowledge proofs and send signals anonymously. A
-                    Semaphore ID represents a user in a group and is made up of two secret values (trapdoor and
-                    nullifier) and one public value (identity commitment).
-                    <br />
-                    <br /> The Poseidon hash of the identity nullifier and trapdoor is called the identity secret, and
-                    its hash is the identity commitment. An identity commitment, like an Ethereum address, is a public
-                    value used in Semaphore groups to represent the identity of a group member. The secret values are
-                    like Ethereum private keys and are used to authenticate signals.
-                </p>
-            )
+            answer: "Semaphore IDs contain 3 parts: Trapdoor and Nullifier to avoid fraud and Commitment to interact publicly."
         },
         {
             id: 2,
-            question: "How does Semaphore provide privacy?",
-            answer: (
-                <p>
-                    Semaphore uses zero-knowledge proofs to prove a signal or message was sent from a member of a group
-                    without revealing who that member was. The identity of the member remains secret because the zk
-                    proof only shows the message was validly signed, but never reveals who it was signed by.
-                    <br />
-                    <br />
-                    Users remain anonymous because signals and identities are not associated in any way.
-                </p>
-            )
+            question: "How am I anonymous?",
+            answer: "Semaphore IDs contain 3 parts: Trapdoor and Nullifier to avoid fraud and Commitment to interact publicly."
         },
         {
             id: 3,
-            question: "What’s an anonymity set?",
-            answer: (
-                <p>
-                    In the context of the TAZ app, an anonymity set is a group containing members that are
-                    indistinguishable from each other. Since ZK proofs break the provable links between actions and
-                    identities, a bigger anonymity set creates more anonymity for the entire group. The anonymity set
-                    for the TAZ app is made up of Devcon VI attendees because only attendees were allowed to join the
-                    Semaphore group.
-                </p>
-            )
+            question: "Why does it take a long time to post?",
+            answer: "Semaphore IDs contain 3 parts: Trapdoor and Nullifier to avoid fraud and Commitment to interact publicly."
         },
         {
             id: 4,
-            question: "Why does it take a long time to post?",
-            answer: (
-                <p>
-                    Posting begins with generating a client-side zero knowledge proof containing the poster’s signal or
-                    message. To preserve anonymity, the proof is sent to a third-party account on the backend, which
-                    then submits an Ethereum transaction with the proof.
-                    <br />
-                    <br />
-                    Proof generation takes only a few seconds, but the time required for the proof to be confirmed by
-                    the network and verified by the smart contract depends on the blockchain.
-                </p>
-            )
-        },
-        {
-            id: 5,
             question: "Why are you using a centralized backend?",
-            answer: (
-                <p>
-                    A zero-knowledge proof must be sent to the Ethereum network, but if users generating the proofs used
-                    their own Ethereum address to send the transaction, it would be possible to link their address to
-                    their proof, making it no longer anonymous.
-                    <br />
-                    <br />A third-party account is used to send proofs on-chain without linking the proofs to any user’s
-                    Ethereum address – preserving anonymity.
-                </p>
-            )
-        },
-        {
-            id: 6,
-            question: "How secure is my Semaphore ID on the TAZ app?",
-            answer: (
-                <p>
-                    Your Semaphore ID will be generated and saved locally in your device. Once your Semaphore ID is
-                    created, its secret values are saved in your browser’s local storage where they can also be exported
-                    to your device as a QR code.
-                    <br />
-                    <br />
-                    Please note the Devcon VI TAZ applications are an experiment. Your Semaphore ID is not encrypted and
-                    we are taking minimal security precautions.
-                </p>
-            )
+            answer: "Semaphore IDs contain 3 parts: Trapdoor and Nullifier to avoid fraud and Commitment to interact publicly."
         }
     ]
 
