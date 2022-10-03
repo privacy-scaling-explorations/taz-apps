a3b7552c95e42bfbc0a5fc0c31870d2510d104af
Rowdy <mroddy5280@gmail.com>
2022-10-03 15:20:11 -0600

    UI changes for tv

--- apps/web-app/components/TazBooth/BoothArtboardCard/index.tsx
+++ apps/web-app/components/TazBooth/BoothArtboardCard/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 
-import { RiLiveLine } from "react-icons/ri"
+import { RiFocus2Line } from "react-icons/ri"
 
 const BoothArtboardCard = ({ canvas }: { canvas: { canvasId: number; tiles: string[] } }) => {
     const emptyTiles = canvas.tiles.filter((tile: string) => tile.length === 0)
     return (
-        <div className="flex flex-col h-[470px] w-1/4 mx-8 p-7 bg-brand-beige2 border border-brand-gray2 shadow-[-20px_15px_0px_0px_rgba(184,174,167)]">
+        <div className="flex flex-col h-[470px] w-1/4 mx-8 p-7 bg-brand-beige2 shadow-[-20px_15px_0px_0px_rgba(184,174,167)]">
             <div className="grow flex flex-wrap bg-white mb-4">
                 {canvas.tiles.map((tile: string, index: number) => (
                     <picture key={index} className="w-1/3 h-1/3">
@@ -14,10 +14,21 @@ const BoothArtboardCard = ({ canvas }: { canvas: { canvasId: number; tiles: stri
                 ))}
             </div>
             <div className="flex flex-col h-24">
-                <h1 className="font-extrabold mb-4">Canvas ID #{canvas.canvasId}</h1>
-                <p className="text-xs mb-4">There are still {emptyTiles.length} available tiles</p>
-                <div className="flex items-center w-fit py-1 px-2 rounded bg-brand-red text-brand-beige animate-pulse">
-                    <RiLiveLine className="mr-1" />
+                <h1 className="font-extrabold mb-4">Canvas {canvas.canvasId}</h1>
+                {emptyTiles.length === 9 && (
+                    <p className="text-xs mb-4">This canvas is empty – Join the TAZ app to start drawing!</p>
+                )}
+                {emptyTiles.length === 0 && (
+                    <p className="text-xs mb-4">This complete canvas has been added to the TAZ app gallery!</p>
+                )}
+                {emptyTiles.length !== 0 && emptyTiles.length !== 9 && (
+                    <p className="text-xs mb-4">
+                        {9 - emptyTiles.length} tiles are complete | {emptyTiles.length} open tiles remainThere are
+                        still
+                    </p>
+                )}
+                <div className="flex items-center w-fit py-1 px-2 rounded bg-brand-red text-brand-beige">
+                    <RiFocus2Line className="mr-1" />
                     <p className="text-xs font-normal">LIVE</p>
                 </div>
             </div>
