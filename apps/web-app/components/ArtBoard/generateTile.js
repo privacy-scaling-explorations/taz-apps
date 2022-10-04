2a8ce54bbdb75956f018da8e62e4dfe369c3f0da
Rowdy <mroddy5280@gmail.com>
2022-10-04 08:58:19 -0600

    Small update after merge

--- apps/web-app/components/ArtBoard/generateTile.js
+++ apps/web-app/components/ArtBoard/generateTile.js
@@ -1,29 +1,19 @@
 import React, { forwardRef } from "react"
 
-<<<<<<< HEAD
-const GenerateTile = forwardRef(({ i, tiles, startDrawing }) => (
-    <td className="bg-white border border-slate-200 p-0 ">
-=======
 const GenerateTile = forwardRef(({ i, tiles, startDrawing, userSelectedTile }) => (
-    <td className="bg-white p-0 ">
->>>>>>> 65227f940fd08a8b9e321c19435555e043b216ac
+    <td className="bg-white border border-slate-200 p-0 ">
         <div className="w-[112px] h-[112px]" onClick={() => startDrawing(i)}>
             {tiles[i] ? (
                 <picture>
                     <img alt={`Artboard tile ${i}`} id={`${i}`} src={tiles[i] ? tiles[i] : ""} />
                 </picture>
             ) : (
-<<<<<<< HEAD
                 <div className="w-[112px] h-[112px] flex items-center">
-                    <p className="text-sm text-brand-blue w-full text-center">open tile</p>
-=======
-                <div className="w-[112px] h-[112px] border border-slate-200 border-collapse flex items-center">
                     {userSelectedTile ? (
                         <p className="text-sm text-brand-blue w-full text-center text-opacity-30">open tile</p>
                     ) : (
                         <p className="text-sm text-brand-blue w-full text-center">open tile</p>
                     )}
->>>>>>> 65227f940fd08a8b9e321c19435555e043b216ac
                 </div>
             )}
         </div>
