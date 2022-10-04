9cb49f77aa4e1b44c5fb7490cf3ddc9b60ab9e89
Rowdy <mroddy5280@gmail.com>
2022-10-04 14:36:01 -0600

    Fixed bug with art canvas overflowing out of container on smaller screens

--- apps/web-app/components/ArtBoard/generateTile.js
+++ apps/web-app/components/ArtBoard/generateTile.js
@@ -2,19 +2,15 @@ import React, { forwardRef } from "react"
 
 const GenerateTile = forwardRef(({ i, tiles, startDrawing, userSelectedTile }) => (
     <td className="bg-white border border-slate-200 p-0 ">
-        <div className="w-[112px] h-[112px]" onClick={() => startDrawing(i)}>
+        <div className="w-[100px] h-[100px] flex items-center" onClick={() => startDrawing(i)}>
             {tiles[i] ? (
                 <picture>
                     <img alt={`Artboard tile ${i}`} id={`${i}`} src={tiles[i] ? tiles[i] : ""} />
                 </picture>
+            ) : userSelectedTile ? (
+                <p className="text-sm text-brand-blue w-full text-center text-opacity-30">open tile</p>
             ) : (
-                <div className="w-[112px] h-[112px] flex items-center">
-                    {userSelectedTile ? (
-                        <p className="text-sm text-brand-blue w-full text-center text-opacity-30">open tile</p>
-                    ) : (
-                        <p className="text-sm text-brand-blue w-full text-center">open tile</p>
-                    )}
-                </div>
+                <p className="text-sm text-brand-blue w-full text-center">open tile</p>
             )}
         </div>
     </td>
