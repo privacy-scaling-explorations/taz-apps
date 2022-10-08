68caae4cd232f27003366ebbc867fbb5d3b7bdf6
Rowdy <mroddy5280@gmail.com>
2022-10-08 06:48:53 -0500

    Updated Booth for Streaming data, fixed types

--- apps/web-app/components/TazBooth/BoothContent/index.tsx
+++ apps/web-app/components/TazBooth/BoothContent/index.tsx
@@ -1,13 +1,56 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import faunadb, { Ref, Collection, Handler } from "faunadb"
+import * as Fauna from "faunadb/src/types/values"
 
 import BoothArtboardCard from "../BoothArtboardCard"
 
-const BoothContent = ({ canvases }: { canvases: { canvasId: number; tiles: string[] }[] }) => (
-    <div className="grow flex justify-between items-center px-14 bg-brand-beige">
-        {canvases.map((canvas: any) => (
-            <BoothArtboardCard key={canvas.canvasId} canvas={canvas} />
-        ))}
-    </div>
-)
+import { Canvas, FanaCanvasResponse } from "../../../pages/booth-display"
+
+export type FaunadbStreamVersionEvent = {
+    action: "create" | "update" | "delete"
+    document: FanaCanvasResponse
+    diff: FanaCanvasResponse
+    prev: FanaCanvasResponse
+}
+
+const BoothContent = (initialCanvases: Canvas[]) => {
+    const [canvases, setCanvases] = useState(initialCanvases)
+    const secret = "fnAEvFcKVTACS4C4wTx9Onll1EfhX8dE9mc5xvzk"
+    const client = new faunadb.Client({ secret })
+    const q = faunadb.query
+
+    function getActiveCanvases() {
+        return client.stream
+            .document(q.Ref(q.Collection("ActiveCanvases"), "344764218231226955"))
+            .on("start", async (start) => {
+                console.log("start", start)
+
+                const dbs: FanaCanvasResponse = await client.query<FanaCanvasResponse>(
+                    q.Get(Ref(Collection("ActiveCanvases"), "344764218231226955"))
+                )
+                if (canvases !== dbs.data.canvases) {
+                    setCanvases(dbs.data.canvases)
+                }
+            })
+            .on("version", (version: any) => {
+                console.log("version", version)
+                // setCanvases(version.document.data.canvases)
+            })
+    }
+
+    useEffect(() => {
+        getActiveCanvases().start()
+        return function cleanUp() {
+            getActiveCanvases().close()
+        }
+    }, [])
+
+    return (
+        <div className="grow flex justify-between items-center px-14 bg-brand-beige">
+            {Array.isArray(canvases) &&
+                canvases.map((canvas) => <BoothArtboardCard key={canvas.canvasId} canvas={canvas} />)}
+        </div>
+    )
+}
 
 export default BoothContent
--- apps/web-app/pages/booth-display/index.tsx
+++ apps/web-app/pages/booth-display/index.tsx
@@ -1,53 +1,48 @@
-import faunadb from "faunadb"
-import { useEffect, useState } from "react"
+import React from "react"
+import faunadb, { Collection, Ref } from "faunadb"
+import * as Fauna from "faunadb/src/types/values"
 
 import BoothContent from "../../components/TazBooth/BoothContent"
 import BoothFooter from "../../components/TazBooth/BoothFooter"
 
-export interface Canvas {
+export type Canvas = {
     canvasId: number
     tiles: string[]
 }
 
-const BoothDisplay = () => {
-    const [canvases, setCanvases] = useState([])
-    const secret = "fnAEvFcKVTACS4C4wTx9Onll1EfhX8dE9mc5xvzk"
-    const client = new faunadb.Client({ secret })
-    console.log("secret", secret, client)
-    const { query } = faunadb
-
-    async function getActiveCanvases() {
-        client.stream
-            .document(query.Ref(query.Collection("ActiveCanvases"), "344764218231226955"))
-            .on("start", async (start) => {
-                console.log("start", start)
-
-                const dbs = await client.query<any>(
-                    query.Map(
-                        query.Paginate(query.Match(query.Index("all_active_canvases"))),
-                        query.Lambda("canvasRef", query.Get(query.Var("canvasRef")))
-                    )
-                )
-                console.log("Canvases in component", dbs.data[0].data.canvases)
-                setCanvases(dbs.data[0].data.canvases)
-            })
-            .on("version", (version: any) => {
-                console.log("version", version.document.data.canvases)
-                setCanvases(version.document.data.canvases)
-            })
-            .start()
-    }
-
-    useEffect(() => {
-        getActiveCanvases()
-    }, [])
-
-    return (
-        <div className="flex flex-col h-screen bg-brand-black">
-            <BoothContent canvases={canvases} />
-            <BoothFooter />
-        </div>
-    )
+export type CanvasData = {
+    [canvases: string]: Canvas[]
 }
 
+export type FanaCanvasResponse = Fauna.values.Document<CanvasData>
+
+const BoothDisplay = (props: { canvases: Canvas[] }) => (
+    <div className="flex flex-col h-screen bg-brand-black">
+        <BoothContent initialCanvases={props.canvases} />
+        <BoothFooter />
+    </div>
+)
+
 export default BoothDisplay
+
+export async function getServerSideProps() {
+    // Server Side code that will never reach client
+    const secret = process.env.FAUNA_SECRET_KEY
+    if (typeof secret !== "string") {
+        return { notFound: true }
+    }
+    const client = new faunadb.Client({ secret })
+    const q = faunadb.query
+
+    try {
+        const dbs: FanaCanvasResponse = await client.query<FanaCanvasResponse>(
+            q.Get(Ref(Collection("ActiveCanvases"), "344764218231226955"))
+        )
+        const { canvases } = dbs.data
+        return {
+            props: { canvases }
+        }
+    } catch (err) {
+        return { Error: { err } }
+    }
+}
