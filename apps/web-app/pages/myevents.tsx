9a79093296d6211455f450650710f638b705a8ac
danilowhk <danilowhk@gmail.com>
2023-03-14 03:41:59 -0300

    fix urls

--- apps/web-app/pages/myevents.tsx
+++ apps/web-app/pages/myevents.tsx
@@ -1,10 +1,8 @@
 import { GetServerSideProps } from "next"
 
-import axios from "axios"
 import { FavoritedEventsDTO, ParticipantsDTO, UserDTO } from "../types"
 
 import MyEventsComponent from "../components/MyEvent"
-
 
 type Props = {
     pastEvents: ParticipantsDTO[]
@@ -26,33 +24,33 @@ export default function MyEvents({ pastEvents, attendingEvents, favoritesEvents,
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     try {
-        const usersResult = await axios.get('/api/fetchUsers')
-        const participantsResult = await axios.get('/api/fetchParticipants')
-        const usersFavoriteEventsResult = await axios.get('/api/fetchFavoritedEvents')
+        const usersResult = await fetch(`https://taz-zulalu-web-app.vercel.app/api/fetchUsers`)
+        const participantsResult = await fetch(`https://taz-zulalu-web-app.vercel.app/api/fetchParticipants`)
+        const usersFavoriteEventsResult = await fetch(`https://taz-zulalu-web-app.vercel.app/api/fetchFavoritedEvents`)
 
-        const usersData: any = usersResult.data
-        const participantsData: any = participantsResult.data
-        const usersFavoriteData: any = await usersFavoriteEventsResult.data
+        const usersData: UserDTO[] = await usersResult.json()
+        const participantsData: ParticipantsDTO[] = await participantsResult.json()
+        const usersFavoriteData: FavoritedEventsDTO[] = await usersFavoriteEventsResult.json()
 
         // mocked user for now (fetching user 1 from db)
         // since we implement user authentication in the server side, we need to fetch user dinamically
-        const userEvents = participantsData.filter((item : any) => item.user_id === 1)
-        const favoritesEvents = usersFavoriteData.filter((item : any) => item.user_id === 1)
-        const pastEvents = userEvents.filter((item: any) => {
+        const userEvents = participantsData.filter((item) => item.user_id === 1)
+        const favoritesEvents = usersFavoriteData.filter((item) => item.user_id === 1)
+        const pastEvents = userEvents.filter((item) => {
             const todayDate = new Date().getTime()
             const eventDate = new Date(item.events.endDate).getTime()
             if (todayDate > eventDate) {
                 return item
             }
         })
-        const attendingEvents = userEvents.filter((item : any) => {
+        const attendingEvents = userEvents.filter((item) => {
             const todayDate = new Date().getTime()
             const eventDate = new Date(item.events.endDate).getTime()
             if (todayDate < eventDate) {
                 return item
             }
         })
-        const userInfo = usersData.find((item : any) => item.id === 1)
+        const userInfo = usersData.find((item) => item.id === 1)
 
         return {
             props: { pastEvents, attendingEvents, favoritesEvents, user: userInfo }
