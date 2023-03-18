33baf6db84c334c2bae34c5d1df5e2d70ad39a0e
Tenzin Rose <tenzinrose@gmail.com>
2023-03-17 22:45:44 -0600

    deleted files

--- apps/web-app/pages/complete-identity-proof.tsx
+++ apps/web-app/pages/complete-identity-proof.tsx
@@ -3,23 +3,50 @@ import { useRouter } from "next/router";
 import {
   useSemaphorePassportProof
 } from "@pcd/passport-interface";
+import { sha256} from 'js-sha256';
+import { createClient } from "@supabase/supabase-js"
+const supabaseUrl = "https://polcxtixgqxfuvrqgthn.supabase.co"
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey as string)
 
 const RedirectPage: React.FC = () => {
   const router = useRouter();
 
   const { semaphoreProof } = useSemaphorePassportProof("https://api.pcd-passport.com/semaphore/1");
 
+  const signInWithSemaphoreProof = async (proof: any) => {
+    //
+    // IMPORTANT!!!! User Email must be changed
+    try {
+      // Check if a user with the Semaphore proof as the email already exists
+      const { data, error } = await supabase.auth.signUp({
+        email: "test1@test.com",
+        password: await sha256(JSON.stringify(proof)),
+      });
+      console.log(data, error)
+      if (error) {
+        const  signIn  = await supabase.auth.signInWithPassword({
+          email: "test1@test.com",
+          password: await sha256(JSON.stringify(proof)),
+        });
+        console.log("Signed in:", signIn);
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     // Do any necessary processing here
-    console.log(semaphoreProof);
+    signInWithSemaphoreProof(semaphoreProof)
     console.log("Redirecting to index...");
-    // router.push("/");
+    router.push("/");
   }, [semaphoreProof]);
 
   return (
     <div>
       <h1>Redirecting to index...</h1>
-      <p>You will be redirected in 5 seconds.</p>
     </div>
   );
 };
