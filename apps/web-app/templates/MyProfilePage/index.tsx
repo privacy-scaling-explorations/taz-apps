3bdbaf665c7e9eff6d7a1b1857fd64076651f5c5
danilowhk <danilowhk@gmail.com>
2023-03-20 04:51:44 -0300

    rebase

--- apps/web-app/templates/MyProfilePage/index.tsx
+++ apps/web-app/templates/MyProfilePage/index.tsx
@@ -5,8 +5,8 @@ import BaseTemplate from "../Base"
 const MyProfilePage = () => (
     <BaseTemplate>
         <div className="flex flex-col bg-[#EEEEF0] px-[24px] py-[24px] gap-[16px]">
-            <div className="flex justify-between p-5 bg-white w-full rounded-[8px]">
-                <div className="flex items-center">
+            <div className="flex justify-between p-5 bg-white w-full rounded-[8px] flex-wrap">
+                <div className="flex items-center w-full md:w-auto mb-4 md:mb-0">
                     <div className="flex w-auto gap-2 px-2 py-1 text-[16px] items-center">
                         <NextImage src={"/user-icon-5.svg"} alt="calendar" width={24} height={24} />
                         <p className="font-bold">Samuel</p>
@@ -20,16 +20,16 @@ const MyProfilePage = () => (
                         <p>Ethereum Foundation</p>
                     </div>
                 </div>
-                <div className="flex justify-center items-center">
-                    <button className="text-[#F8FFFE] bg-[#35655F] rounded-[8px] flex flex-row justify-center items-center py-[8px] px-[16px] flex flex-row gap-[8px]">
+                <div className="flex justify-center items-center w-full md:w-auto">
+                    <button className="text-[#F8FFFE] bg-[#35655F] rounded-[8px] flex flex-row justify-center items-center py-[8px] px-[16px] flex flex-row gap-[8px] w-full md:w-auto">
                         <NextImage src={"/pencil.svg"} width={13} height={12} />
                         <p>EDIT</p>
                     </button>
                 </div>
             </div>
-            <div className="flex justify-between h-[100vh]">
-                <div className="p-5 flex flex-col items-start bg-white rounded-[8px] w-4/6 gap-10">
-                    <div className="flex justify-between w-full">
+            <div className="flex flex-col md:flex-row justify-between h-[100vh]">
+                <div className="p-5 flex flex-col items-start bg-white rounded-[8px] w-full md:w-4/6 gap-10">
+                                        <div className="flex justify-between w-full">
                         <div className="flex items-center gap-10">
                             <h1 className="font-semibold text-[40px]">My Sessions</h1>
                             <button className="flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px] h-[40px]">
@@ -168,9 +168,10 @@ const MyProfilePage = () => (
                     </div>
 
                     {/* It ends Here */}
+
                 </div>
-                <div className="flex flex-col pl-5 w-2/6 gap-10">
-                    <div className="flex flex-col p-5 gap-5 bg-white rounded-[8px]">
+                <div className="flex flex-col pl-5 w-full md:w-2/6 gap-10 mt-10 md:mt-0">
+                                        <div className="flex flex-col p-5 gap-5 bg-white rounded-[8px]">
                         <h1 className="text-[24px] font-semibold">My Sessions</h1>
                         <div className="flex gap-1 flex-col">
                             <h1 className="capitalize">My Sessions</h1>
@@ -201,6 +202,7 @@ const MyProfilePage = () => (
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
