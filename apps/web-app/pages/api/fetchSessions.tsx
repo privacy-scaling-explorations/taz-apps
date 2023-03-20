246c7d3e427d5ba34715bd6fdce6c0c435074ed6
Rich Warner <richwarneriii@gmail.com>
2023-03-20 14:42:18 -0600

    new api endpoints for sessions

--- apps/web-app/pages/api/fetchSessions.tsx
+++ apps/web-app/pages/api/fetchSessions.tsx
@@ -12,7 +12,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const response = await supabase
             .from("sessions")
             .select("*, participants (*), favorited_sessions (*)")
-            .eq("id", req.query.sessionId)
             .eq("participants.user_id", userId)
             .eq("favorited_sessions.user_id", userId)
         if (response.error === null) res.status(200).send(response.data)
