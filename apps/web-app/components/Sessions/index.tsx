a6b1f7a77c43fe8d1387d327580fe067233dfcbd
danilowhk <danilowhk@gmail.com>
2023-03-21 02:39:07 -0300

    fix navbar connection check

--- apps/web-app/components/Sessions/index.tsx
+++ apps/web-app/components/Sessions/index.tsx
@@ -151,7 +151,7 @@ const Sessions = ({ event, sessions }: Props) => {
                         </div>
                         <div className="w-full flex flex-row gap-[32px] justify-between items-center">
                             <div className="flex flex-row items-start gap-[8px]">
-                                {item.team_members.map((organizer : any, key : any) => (
+                                {item.team_members?.map((organizer : any, key : any) => (
                                     <div
                                         className="flex flex-row items-center bg-[#E4EAEA] py-[4px] px-[8px] gap-[8px] text-sm rounded-[4px]"
                                         key={key}
