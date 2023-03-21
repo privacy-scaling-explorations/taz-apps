3d41630a06f073e1eeec3a7a3191cc54d120cc0c
samueloh99 <samuel_oh@hotmail.com>
2023-03-21 19:15:43 -0300

    Implemented new design for create session and yarn build

--- apps/web-app/components/AddSessionModal/index.tsx
+++ apps/web-app/components/AddSessionModal/index.tsx
@@ -30,12 +30,6 @@ type NewSessionState = {
     type: string
 }
 
-type TicketsState = {
-    name: string
-    price: string
-    description: string
-}
-
 type Props = {
     isOpen: boolean
     closeModal: (b: boolean) => void
@@ -155,37 +149,17 @@ const AddSessionModal = ({ isOpen, closeModal, eventId }: Props) => {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <Dialog.Panel className="flex flex-col min-h-[200px] h-full w-5/6 h-[100%] overflow-scroll md:overflow-hidden md:h-auto max-w-full transform rounded-lg border-brand-blue border-2 bg-white text-left align-middle shadow-xl transition-all">
-                                <div className="p-4 bg-brand-beige border-b-2 border-brand-blue">
-                                    <button
-                                        type="button"
-                                        className="flex items-center focus:outline-none focus-visible:ring-2 focus-visible:ring-brand-beige2"
-                                        onClick={() => closeModal(false)}
-                                    >
-                                        <svg
-                                            className="mr-2"
-                                            width="18"
-                                            height="17"
-                                            viewBox="0 0 18 17"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
+                            <Dialog.Panel className="flex flex-col h-full w-5/6 overflow-y-scroll max-w-full transform rounded-lg bg-white text-left align-middle  transition-all">
+                                <div className="w-full h-full py-5 px-10">
+                                    <div className="flex w-full justify-between items-center">
+                                        <h1 className="text-[24px] font-[600]">Session Info (for the public)</h1>
+                                        <div
+                                            onClick={() => closeModal(false)}
+                                            className="cursor-pointer flex items-center border-2 border-black justify-center w-[25px] h-[25px] rounded-full"
                                         >
-                                            <path
-                                                transform="translate(5,5)"
-                                                d="M7.25467 1.49196C7.55767 1.20954 7.57439 0.73496 7.29194 0.431952C7.00957 0.128937 6.53497 0.112242 6.23197 0.394655L4.03739 2.44007L1.99196 0.24548C1.70954 -0.0575276 1.23496 -0.0742224 0.931952 0.20819C0.628937 0.490603 0.612242 0.96518 0.894655 1.2682L2.94007 3.46277L0.74548 5.50824C0.442472 5.79062 0.425778 6.26522 0.70819 6.56822C0.990603 6.87122 1.46518 6.88794 1.7682 6.60549L3.96277 4.56009L6.00824 6.75467C6.29062 7.05767 6.76522 7.07439 7.06822 6.79194C7.37122 6.50957 7.38794 6.03497 7.10549 5.73197L5.06009 3.53739L7.25467 1.49196Z"
-                                                fill="#435C6C"
-                                            />
-                                            <path
-                                                fillRule="evenodd"
-                                                clipRule="evenodd"
-                                                d="M0.75 8.5C0.75 3.94365 4.44365 0.25 9 0.25C13.5563 0.25 17.25 3.94365 17.25 8.5C17.25 13.0563 13.5563 16.75 9 16.75C4.44365 16.75 0.75 13.0563 0.75 8.5ZM9 15.25C5.27208 15.25 2.25 12.228 2.25 8.5C2.25 4.77208 5.27208 1.75 9 1.75C12.728 1.75 15.75 4.77208 15.75 8.5C15.75 12.228 12.728 15.25 9 15.25Z"
-                                                fill="#435C6C"
-                                            />
-                                        </svg>
-                                        cancel
-                                    </button>
-                                </div>
-                                <div className="flex flex-col h-full p-5 ">
+                                            X
+                                        </div>
+                                    </div>
                                     <ModalSteps steps={steps} />
                                     {steps === 1 && (
                                         <Step1
@@ -194,6 +168,7 @@ const AddSessionModal = ({ isOpen, closeModal, eventId }: Props) => {
                                             setSteps={setSteps}
                                         />
                                     )}
+
                                     {steps === 2 && (
                                         <Step2
                                             setSteps={setSteps}
@@ -203,6 +178,7 @@ const AddSessionModal = ({ isOpen, closeModal, eventId }: Props) => {
                                             setNewSession={setNewSession}
                                         />
                                     )}
+
                                     {steps === 3 && (
                                         <Step3
                                             setSteps={setSteps}
