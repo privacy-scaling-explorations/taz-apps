30e731c1b8ba231bf03b936042206db0a8ddb852
Falco Rodenburg <falcorodenburg@gmail.com>
2023-03-22 18:31:43 +0700

    session modal on calendar page

---
+++ apps/web-app/components/CalendarSessionModal/Step3.tsx
@@ -0,0 +1,131 @@
+import { useState } from "react"
+
+type NewSessionState = {
+    name: string
+    organizers: string[]
+    team_members: { name: string; role: string }[]
+    startDate: Date
+    endDate: Date
+    startTime: string
+    endTime: string
+    location: string
+    tags: string[]
+    info: string
+    eventId: number
+    hasTicket: boolean
+    format: string
+    level: string
+    equipment: string
+    track: string
+    type: string
+}
+
+type Props = {
+    setSteps: (step: number) => void
+    amountTickets: string
+    setAmountTickets: Function
+    newSession: NewSessionState
+    setNewSession: (newEvent: NewSessionState) => void
+}
+
+const Step3 = ({ setSteps, amountTickets, setAmountTickets, newSession, setNewSession }: Props) => {
+    const handleSubmit = async () => {
+        setSteps(4)
+    }
+    return (
+        <div className="flex flex-col w-full gap-5">
+            <div className="flex flex-col my-2 gap-5">
+                <h1 className="text-[18px] font-[600]">How do you want people to show attendance?</h1>
+                <div className="flex flex-col gap-2">
+                    <div className="flex items-center gap-4">
+                        <input
+                            type="radio"
+                            id="ticket"
+                            value="ticket"
+                            className="w-[24px] h-[24px]"
+                            checked={newSession.hasTicket}
+                            onChange={() => setNewSession({ ...newSession, hasTicket: !newSession.hasTicket })}
+                        />
+                        <label htmlFor="ticket">Ticket</label>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <input
+                            type="radio"
+                            id="RSVP"
+                            value="RSVP"
+                            className="w-[24px] h-[24px]"
+                            checked={!newSession.hasTicket}
+                            onChange={() => setNewSession({ ...newSession, hasTicket: !newSession.hasTicket })}
+                        />
+                        <label htmlFor="RSVP">RSVP</label>
+                    </div>
+                </div>
+            </div>
+            {newSession.hasTicket ?
+            <div className="flex flex-col gap-1 my-1 w-full">
+                <label htmlFor="amount" className="font-[600]">
+                    How many tickets?
+                </label>
+                <input
+                    className="border-[#C3D0CF] bg-white border-2 p-1 rounded-[8px] h-[42px]"
+                    id="amount"
+                    type="number"
+                    min="0"
+                    max="1000"
+                    value={amountTickets}
+                    onChange={(e) => setAmountTickets(e.target.value)}
+                />
+            </div>
+            : ""}
+            <div className="flex flex-col my-2">
+                <label htmlFor="equipment" className="font-[600]">
+                    Do you need equipment?
+                </label>
+                <input
+                    className="border-[#C3D0CF] bg-white border-2 p-1 rounded-[8px] h-[42px]"
+                    id="equipment"
+                    type="text"
+                    value={newSession.equipment}
+                    onChange={(e) => setNewSession({ ...newSession, equipment: e.target.value })}
+                />
+            </div>
+
+            <div className="flex flex-col my-2">
+                <label htmlFor="info" className="font-[600]">
+                    Is there anything else we need to know?
+                </label>
+                <textarea
+                    className="border-[#C3D0CF] border-2 p-1 rounded-[8px] h-[150px]"
+                    placeholder="Your answers will be shared with Zulalu Organizers"
+                    name="info"
+                    id="info"
+                    rows={5}
+                    maxLength={2000}
+                    value={newSession.info}
+                    onChange={(e) => setNewSession({ ...newSession, info: e.target.value })}
+                />
+                <div className="flex w-full justify-end">
+                    <h1 className="text-[14px] text-[#AAAAAA]">Max 2000 characters</h1>
+                </div>
+            </div>
+            <div className="w-full flex flex-col gap-5 justify-center items-center mt-5">
+                <button
+                    type="button"
+                    className="w-full flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px]"
+                    onClick={() => setSteps(2)}
+                >
+                    BACK
+                </button>
+                <button
+                    type="button"
+                    className="w-full flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px]"
+                    onClick={handleSubmit}
+                >
+                    NEXT
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Step3
---
+++ apps/web-app/components/CalendarSessionModal/Step4.tsx
@@ -0,0 +1,86 @@
+import NextImage from "next/image"
+import Loading from "../Loading"
+
+type NewSessionState = {
+    name: string
+    organizers: string[]
+    team_members: { name: string; role: string }[]
+    startDate: Date
+    endDate: Date
+    startTime: string
+    endTime: string
+    location: string
+    tags: string[]
+    info: string
+    eventId: number
+    hasTicket: boolean
+    format: string
+    level: string
+    equipment: string
+    track: string
+    type: string
+}
+
+type Props = {
+    setSteps: (step: number) => void
+    newSession: NewSessionState
+    handleSubmit: Function
+    isLoading: boolean
+}
+
+const Step4 = ({ setSteps, newSession, handleSubmit, isLoading }: Props) => {
+    const day = newSession.startDate
+        ? new Date(newSession.startDate).toLocaleDateString("en-US", { day: "numeric" })
+        : ""
+
+    const month = newSession.startDate
+        ? new Date(newSession.startDate).toLocaleDateString("en-US", { month: "long" })
+        : ""
+
+    const weekday = newSession.startDate
+        ? new Date(newSession.startDate).toLocaleDateString("en-US", { weekday: "long" })
+        : ""
+
+    return (
+        <div className="flex flex-col w-full gap-8 bg-white rounded-lg">
+            <div className="flex flex-col gap-4">
+                <h1 className="text-[18px] font-bold uppercase ">{newSession.name}</h1>
+                <h1 className="text-[18px]">{newSession.info}</h1>
+
+                <div className="flex items-center gap-2">
+                    <NextImage src="/vector-calendar.svg" width={20} height={20} />
+                    <h1>{`${weekday}, ${month} ${day}th`}</h1>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <NextImage src="/vector-clock.svg" width={20} height={20} />
+                    <h1>{newSession.startTime}</h1>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <NextImage src="/vector-location.svg" width={20} height={20} />
+                    <h1>{newSession.location}</h1>
+                </div>
+            </div>
+            <div className="w-full flex flex-col gap-5 justify-center items-center mt-5">
+                <button
+                    type="button"
+                    className="w-full flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px]"
+                    onClick={() => setSteps(3)}
+                >
+                    BACK
+                </button>
+                <button
+                    type="button"
+                    disabled={isLoading}
+                    className="w-full flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px]"
+                    onClick={() => handleSubmit()}
+                >
+                    {isLoading ? <Loading size="xs" /> : "CREATE SESSION!"}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Step4
