2856cb0c2af11242f8b93fd6c4eec730da7d5eba
Falco Rodenburg <falcorodenburg@gmail.com>
2023-03-23 14:42:56 +0700

    delete session flow

---
+++ apps/web-app/pages/api/deleteSession.tsx
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { createClient } from "@supabase/supabase-js"
+// import fetch from "node-fetch"
+
+const supabaseUrl = "https://polcxtixgqxfuvrqgthn.supabase.co"
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey as string)
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        const { id } = req.body
+
+        const response = await supabase.from("sessions").delete().match({ id: id })
+
+        console.log("Response: ", response)
+
+        res.status(201).json("Event created")
+    } catch (err: any) {
+        console.log("error: ", err)
+        res.status(500).json({ statusCode: 500, message: err.message })
+    }
+}
