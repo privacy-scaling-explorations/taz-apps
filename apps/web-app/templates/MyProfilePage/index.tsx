1ba59f2e1d12f49e2737bbf62cbd2a469ec42a08
m <13090422+marciob@users.noreply.github.com>
2023-03-24 03:25:14 -0300

    making myprofile responsive

--- apps/web-app/templates/MyProfilePage/index.tsx
+++ apps/web-app/templates/MyProfilePage/index.tsx
@@ -41,12 +41,13 @@ const MyProfilePage = ({ events, sessions }: Props) => {
         selectedOpt.length > 0
             ? sortByDate.filter((item) => selectedOpt.includes(item.events.name.replace("\n", "")))
             : sortByDate
-
     return (
         <BaseTemplate>
-            <div className="flex flex-col bg-[#EEEEF0] px-[24px] py-[24px] gap-[16px]">
-                <div className="flex justify-between p-5 bg-white w-full rounded-[8px] flex-wrap">
-                    <div className="flex items-center w-full md:w-auto mb-4 md:mb-0">
+            {/* <div className="flex flex-col bg-[#EEEEF0] px-6 sm:px-12 md:px-[24px] py-6 sm:py-12 md:py-[24px] gap-4 sm:gap-8 md:gap-[16px]"> */}
+            <div className="flex flex-col bg-[#EEEEF0] px-4 md:px-[24px] py-4 md:py-[24px] gap-4 md:gap-[16px]">
+                <div className="flex flex-col sm:flex-row justify-between p-5 bg-white w-full rounded-[8px] flex-wrap">
+                    {/* <div className="flex items-center w-full md:w-auto mb-4 md:mb-0"> */}
+                    <div className="flex flex-col md:flex-row items-start md:items-center w-full md:w-auto mb-4 md:mb-0 space-y-4 md:space-y-0 md:space-x-4">
                         <div className="flex w-auto gap-2 px-2 py-1 text-[16px] items-center">
                             <NextImage src={"/user-icon-5.svg"} alt="calendar" width={24} height={24} />
                             <p className="font-bold capitalize">{userInfo && userInfo.userName}</p>
@@ -60,12 +61,6 @@ const MyProfilePage = ({ events, sessions }: Props) => {
                             <p>Ethereum Foundation</p>
                         </div>
                     </div>
-                    {/* <div className="flex justify-center items-center w-full md:w-auto">
-                        <button className="text-[#F8FFFE] bg-[#35655F] rounded-[8px] flex flex-row justify-center items-center py-[8px] px-[16px] flex flex-row gap-[8px] w-full md:w-auto">
-                            <NextImage src={"/pencil.svg"} width={13} height={12} />
-                            <p>EDIT</p>
-                        </button>
-                    </div> */}
                 </div>
                 <CalendarSessionModal
                     closeModal={setOpenAddSessionModal}
@@ -75,29 +70,19 @@ const MyProfilePage = ({ events, sessions }: Props) => {
                 />
                 <div className="flex flex-col md:flex-row justify-between h-full">
                     <div className="p-5 flex flex-col items-start bg-white rounded-[8px] w-full md:w-4/6 gap-10">
-                        <div className="flex justify-between w-full">
-                            <div className="flex items-center gap-10">
-                                <h1 className="font-semibold text-[40px]">My Sessions</h1>
+                        <div className="flex flex-wrap md:flex-nowrap justify-between w-full">
+                            <div className="flex flex-wrap md:flex-nowrap items-center gap-10">
+                                <h1 className="font-semibold text-3xl md:text-[40px] mb-0 md:mb-0">My Sessions</h1>
                                 <button
                                     onClick={() => setOpenAddSessionModal(true)}
-                                    className="flex flex-row font-[600] justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px] h-[40px]"
+                                    className="flex flex-row font-semibold justify-center items-center py-[8px] px-[16px] gap-[8px] bg-[#35655F] rounded-[8px] text-white text-[16px] h-[40px]"
                                 >
                                     CREATE SESSION
                                 </button>
                             </div>
-                            {/* <div className="flex items-center">
-                                <button className="bg-white border border-primary text-zulalu-primary font-[600] py-[8px] px-[16px] gap-[8px] text-[16px] rounded-[8px] flex flex-row justify-center items-center">
-                                    <p>ALL MY SESSIONS</p>
-                                    <NextImage src={"/arrow-down.svg"} width={8} height={4} />
-                                </button>
-                            </div> */}
-                        </div>
-
-                        <div className="flex flex-col gap-2 justify-between w-full">
-                            <Sessions sessions={filteredSessions} showStartDate={true} />
                         </div>
                     </div>
-                    <div className="flex flex-col pl-5 w-full md:w-2/6 gap-10 mt-10 md:mt-0">
+                    <div className="flex flex-col pl-0 md:pl-5 w-full md:w-2/6 gap-10 mt-10 md:mt-0">
                         <div className="flex flex-col p-5 gap-5 bg-white rounded-[8px]">
                             <h1 className="text-[24px] font-semibold">My Sessions</h1>
                             <div className="flex gap-2 flex-col items-start justify-center">
