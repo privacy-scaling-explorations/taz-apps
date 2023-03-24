0f650987cd8de09c1a65b0a9dfb88933b433fc2c
Falco Rodenburg <falcorodenburg@gmail.com>
2023-03-24 20:12:20 +0700

    step-dots fixed

--- apps/web-app/components/CalendarSessionModal/ModalSteps.tsx
+++ apps/web-app/components/CalendarSessionModal/ModalSteps.tsx
@@ -5,22 +5,22 @@ type Props = {
 const ModalSteps = ({ steps }: Props) => (
     <div className="flex justify-start gap-3 w-full my-10">
         <div
-            className={`w-[10px] bg-zulalu-primary h-[10px] rounded-full ${
+            className={`w-[10px] h-[10px] rounded-full ${
                 steps >= 1 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
             }`}
         />
         <div
-            className={`w-[10px] bg-zulalu-primary h-[10px] rounded-full ${
+            className={`w-[10px] h-[10px] rounded-full ${
                 steps >= 2 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
             }`}
         />
         <div
-            className={`w-[10px] bg-zulalu-primary h-[10px] rounded-full ${
+            className={`w-[10px] h-[10px] rounded-full ${
                 steps >= 3 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
             }`}
         />
         <div
-            className={`w-[10px] bg-zulalu-primary h-[10px] rounded-full ${
+            className={`w-[10px] h-[10px] rounded-full ${
                 steps >= 4 ? "bg-zulalu-primary" : "bg-[#D3DDDC]"
             }`}
         />
