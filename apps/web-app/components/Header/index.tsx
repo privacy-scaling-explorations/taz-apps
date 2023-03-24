8326e6ae6213429d0ac3405f824355c9ad909806
danilowhk <danilowhk@gmail.com>
2023-03-23 22:42:11 -0300

    fix url

--- apps/web-app/components/Header/index.tsx
+++ apps/web-app/components/Header/index.tsx
@@ -60,7 +60,7 @@ const Header = () => {
             console.log("log my proof", participant1)
             const response = await axios({
                 method: "post",
-                url: "https:localhost:3000/api/passport-user-login/",
+                url: "https://zuzalu-event.vercel.app/api/passport-user-login/",
                 data: participant1,
                 headers: {
                     "Content-Type": "application/json"
