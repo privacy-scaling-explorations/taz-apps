23d052f1217a196de6aa2c15eda26221d8b0a76e
Rich Warner <richwarneriii@gmail.com>
2023-03-23 22:11:23 -0600

    revert back

--- apps/web-app/pages/api/passport-user-login.tsx
+++ apps/web-app/pages/api/passport-user-login.tsx
@@ -11,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         // IMPORTANT!!!! User Email must be changed
         // Validate Proof of user before interacting with DB
         const { uuid, commitment, email, name, role, residence, order_id } = identity
-        const password: string = "q8Vonx$rdnmAlO7bSdoylwZ#oLoH" // process.env.SINGLE_KEY_LOGIN
+        const password: any = process.env.SINGLE_KEY_LOGIN
         try {
             const signIn = await supabase.auth.signInWithPassword({
                 email,
@@ -21,34 +21,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 res.status(200).json("User signed in!")
             }
             if (signIn.error) {
-                res.status(400).json(`Error with sign in: ${signIn.error.message}`)
+                const signUpResponse = await supabase.auth.signUp({
+                    email,
+                    password,
+                    options: {
+                        data: {
+                            uuid,
+                            commitment,
+                            email,
+                            name,
+                            role,
+                            residence,
+                            order_id
+                        }
+                    }
+                })
 
-                // const signUpResponse = await supabase.auth.signUp({
-                //     email,
-                //     password,
-                //     options: {
-                //         data: {
-                //             uuid,
-                //             commitment,
-                //             email,
-                //             name,
-                //             role,
-                //             residence,
-                //             order_id
-                //         }
-                //     }
-                // })
-
-                // console.log("sign up", signUpResponse)
-                // if (signUpResponse.data.user) {
-                //     res.status(200).json("User signed up!")
-                // } else {
-                //     res.status(400).json("Error with sign up")
-                // }
+                console.log("sign up", signUpResponse)
+                if (signUpResponse.data.user) {
+                    res.status(200).json("User signed up!")
+                } else {
+                    res.status(400).json("Error with sign up")
+                }
             }
         } catch (error) {
-            // console.log(error)
-            res.status(500).json(`General error: ${error}`)
+            console.log(error)
         }
     }
     signInWithSemaphoreProof(req.body)
