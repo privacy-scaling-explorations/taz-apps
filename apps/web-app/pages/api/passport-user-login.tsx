31e9af70e46f6b9612fd5564b1f1d55a749423ca
Rich Warner <richwarneriii@gmail.com>
2023-03-23 21:01:53 -0600

    reset to working login

--- apps/web-app/pages/api/passport-user-login.tsx
+++ apps/web-app/pages/api/passport-user-login.tsx
@@ -1,79 +1,52 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "@supabase/supabase-js";
-import { useSemaphorePassportProof } from "@pcd/passport-interface";
-import { sha256 } from "js-sha256";
-
+import { NextApiRequest, NextApiResponse } from "next"
+import { createClient } from "@supabase/supabase-js"
+import { useSemaphorePassportProof } from "@pcd/passport-interface"
+import { sha256 } from "js-sha256"
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs"
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  console.log("request body", req.body);
-  const supabase = createServerSupabaseClient({ req, res })
-  const signInWithSemaphoreProof = async (identity: any) => {
-    console.log("Login identity", identity)
-    const {
-      uuid,
-      commitment,
-      email,
-      name,
-      role,
-      residence,
-      order_id,
-    } = identity;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log("request body", req.body)
+    const supabase = createServerSupabaseClient({ req, res })
+    const signInWithSemaphoreProof = async (identity: any) => {
+        // IMPORTANT!!!! User Email must be changed
+        // Validate Proof of user before interacting with DB
+        const { uuid, commitment, email, name, role, residence, order_id } = identity
+        const password: any = process.env.SINGLE_KEY_LOGIN
+        try {
+            const signIn = await supabase.auth.signInWithPassword({
+                email,
+                password
+            })
+            if (signIn.data.user) {
+                res.status(200).json("User signed in!")
+            }
+            if (signIn.error) {
+                const signUpResponse = await supabase.auth.signUp({
+                    email,
+                    password,
+                    options: {
+                        data: {
+                            uuid,
+                            commitment,
+                            email,
+                            name,
+                            role,
+                            residence,
+                            order_id
+                        }
+                    }
+                })
 
-    console.log("Login email", email)
-    const password: any = process.env.SINGLE_KEY_LOGIN;
-    console.log("Login password", password)
-
-    try {
-      // Check if email is registered
-      const { data: users } = await supabase.from("auth.users").select("email").eq("email", email);
-
-      if (users && users.length > 0) {
-        // If registered, try sign in
-        console.log("signing in data", email, password)
-        const signIn = await supabase.auth.signInWithPassword({
-          email,
-          password,
-        });
-
-        if (signIn.data.user) {
-          res.status(200).json("User signed in!");
-        } else {
-          res.status(400).json("Error with sign in");
+                console.log("sign up", signUpResponse)
+                if (signUpResponse.data.user) {
+                    res.status(200).json("User signed up!")
+                } else {
+                    res.status(400).json("Error with sign up")
+                }
+            }
+        } catch (error) {
+            console.log(error)
         }
-      } else {
-        // If not registered, try sign up
-        console.log("signing up data", email, password, uuid, commitment, email, name, role, residence, order_id)
-        const signUpResponse = await supabase.auth.signUp({
-          email,
-          password,
-          options: {
-            data: {
-              uuid,
-              commitment,
-              email,
-              name,
-              role,
-              residence,
-              order_id,
-            },
-          },
-        });
-
-        if (signUpResponse.data.user) {
-          res.status(200).json("User signed up!");
-        } else {
-          res.status(400).json("Error with sign up");
-        }
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json("Error occurred");
     }
-  };
-
-  signInWithSemaphoreProof(req.body);
+    signInWithSemaphoreProof(req.body)
 }
