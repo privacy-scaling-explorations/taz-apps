dff22c9c04120b50e32a68538640921be4ffcecf
samueloh99 <samuel_oh@hotmail.com>
2023-03-24 06:16:54 -0300

    Changes

--- apps/web-app/pages/calendar-full.tsx
+++ apps/web-app/pages/calendar-full.tsx
@@ -1,4 +1,7 @@
 import { GetServerSideProps } from "next"
+import axios from "axios"
+
+import { parseCookies } from "nookies"
 
 import { EventsDTO, SessionsDTO } from "../types"
 import CalendarPage from "../templates/CalendarPage"
@@ -12,7 +15,7 @@ export default function Event({ sessions, events }: Props) {
     return <CalendarPage sessions={sessions} events={events} />
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     try {
         const url = process.env.URL_TO_FETCH
 
@@ -20,12 +23,20 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
         const events: EventsDTO[] = await eventsResponse.json()
 
-        const sessionsResponse = await fetch(`${url}/api/fetchSessions`)
+        let sessionsRes: SessionsDTO[] = []
 
-        const sessions: SessionsDTO[] = await sessionsResponse.json()
+        await axios
+            .get(`${url}/api/fetchSessions`, {
+                headers: {
+                    Cookie: req.headers.cookie || "" // Pass cookies from the incoming request
+                }
+            })
+            .then((response: any) => {
+                sessionsRes = response.data
+            })
 
         return {
-            props: { sessions, events }
+            props: { sessions: sessionsRes, events }
         }
     } catch (error) {
         res.statusCode = 404
